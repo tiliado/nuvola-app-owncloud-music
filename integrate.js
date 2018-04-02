a0ba25e594a30d9e5fb207f653f3f0e0387e0073
Jiří Janoušek <janousek.jiri@gmail.com>
2018-04-02 18:52:50 +0200

    Port to Standard JS code style
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- integrate.js
+++ integrate.js
@@ -1,15 +1,15 @@
 /*
- * Copyright 2016 Jiří Janoušek <janousek.jiri@gmail.com>
+ * Copyright 2016-2018 Jiří Janoušek <janousek.jiri@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- * 
+ * modification, are permitted provided that the following conditions are met:
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
- * 
+ *    and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,181 +22,160 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-"use strict";
+'use strict';
 
-(function(Nuvola)
-{
-var DEFAULT_ADDRESS = "http://localhost/owncloud/index.php/apps/music/";
-var ADDRESS = "app.address";
-var ADDRESS_TYPE = "app.address_type";
-var ADDRESS_DEFAULT = "default";
-var ADDRESS_CUSTOM = "custom";
+(function (Nuvola) {
+  var DEFAULT_ADDRESS = 'http://localhost/owncloud/index.php/apps/music/'
+  var ADDRESS = 'app.address'
+  var ADDRESS_TYPE = 'app.address_type'
+  var ADDRESS_DEFAULT = 'default'
+  var ADDRESS_CUSTOM = 'custom'
 
-// Translations
-var _ = Nuvola.Translate.gettext;
+  // Translations
+  var _ = Nuvola.Translate.gettext
 
-// Create media player component
-var player = Nuvola.$object(Nuvola.MediaPlayer);
+  // Create media player component
+  var player = Nuvola.$object(Nuvola.MediaPlayer)
 
-// Handy aliases
-var PlaybackState = Nuvola.PlaybackState;
-var PlayerAction = Nuvola.PlayerAction;
+  // Handy aliases
+  var PlaybackState = Nuvola.PlaybackState
+  var PlayerAction = Nuvola.PlayerAction
 
-// Create new WebApp prototype
-var WebApp = Nuvola.$WebApp();
+  // Create new WebApp prototype
+  var WebApp = Nuvola.$WebApp()
 
-WebApp._onInitAppRunner = function(emitter)
-{
-    Nuvola.WebApp._onInitAppRunner.call(this, emitter);
-    Nuvola.config.setDefault(ADDRESS_TYPE, ADDRESS_DEFAULT);
-    Nuvola.config.setDefault(ADDRESS, DEFAULT_ADDRESS);
-    Nuvola.core.connect("InitializationForm", this);
-    Nuvola.core.connect("PreferencesForm", this);
-}
+  WebApp._onInitAppRunner = function (emitter) {
+    Nuvola.WebApp._onInitAppRunner.call(this, emitter)
+    Nuvola.config.setDefault(ADDRESS_TYPE, ADDRESS_DEFAULT)
+    Nuvola.config.setDefault(ADDRESS, DEFAULT_ADDRESS)
+    Nuvola.core.connect('InitializationForm', this)
+    Nuvola.core.connect('PreferencesForm', this)
+  }
 
-WebApp._onInitializationForm = function(emitter, values, entries)
-{
-    if (!Nuvola.config.hasKey(ADDRESS_TYPE))
-        this.appendPreferences(values, entries);
-}
+  WebApp._onInitializationForm = function (emitter, values, entries) {
+    if (!Nuvola.config.hasKey(ADDRESS_TYPE)) {
+      this.appendPreferences(values, entries)
+    }
+  }
 
-WebApp._onPreferencesForm = function(emitter, values, entries)
-{
-    this.appendPreferences(values, entries);
-}
+  WebApp._onPreferencesForm = function (emitter, values, entries) {
+    this.appendPreferences(values, entries)
+  }
 
-WebApp.appendPreferences = function(values, entries)
-{
-    values[ADDRESS_TYPE] = Nuvola.config.get(ADDRESS_TYPE);
-    values[ADDRESS] = Nuvola.config.get(ADDRESS);
-    entries.push(["header", _("ownCloud Music")]);
-    entries.push(["label", _("Specify the address of your ownCloud Music server")]);
-    entries.push(["option", ADDRESS_TYPE, ADDRESS_DEFAULT,
-        _("Default adress:") + " " + DEFAULT_ADDRESS, null, [ADDRESS]]);
-    entries.push(["option", ADDRESS_TYPE, ADDRESS_CUSTOM,
-        _("Custom address:"), [ADDRESS], null]);
-    entries.push(["string", ADDRESS]);
-}
+  WebApp.appendPreferences = function (values, entries) {
+    values[ADDRESS_TYPE] = Nuvola.config.get(ADDRESS_TYPE)
+    values[ADDRESS] = Nuvola.config.get(ADDRESS)
+    entries.push(['header', _('ownCloud Music')])
+    entries.push(['label', _('Specify the address of your ownCloud Music server')])
+    entries.push(['option', ADDRESS_TYPE, ADDRESS_DEFAULT,
+      _('Default adress:') + ' ' + DEFAULT_ADDRESS, null, [ADDRESS]])
+    entries.push(['option', ADDRESS_TYPE, ADDRESS_CUSTOM,
+      _('Custom address:'), [ADDRESS], null])
+    entries.push(['string', ADDRESS])
+  }
 
-WebApp._onHomePageRequest = function(emitter, result)
-{
+  WebApp._onHomePageRequest = function (emitter, result) {
     result.url = (Nuvola.config.get(ADDRESS_TYPE) === ADDRESS_CUSTOM)
-        ? Nuvola.config.get(ADDRESS) : DEFAULT_ADDRESS;
-}
+      ? Nuvola.config.get(ADDRESS) : DEFAULT_ADDRESS
+  }
 
-// Initialization routines
-WebApp._onInitWebWorker = function(emitter)
-{
-    Nuvola.WebApp._onInitWebWorker.call(this, emitter);
-    
-    if (!Nuvola.VERSION || Nuvola.VERSION < 30100)
-        alert(Nuvola.format(
-            "This web app requires Nuvola Player 3.1.0 or greater. Your version is {1}.{2}.{3}",
-            Nuvola.VERSION_MAJOR, Nuvola.VERSION_MINOR, Nuvola.VERSION_BUGFIX));
-    
-    var state = document.readyState;
-    if (state === "interactive" || state === "complete")
-        this._onPageReady();
-    else
-        document.addEventListener("DOMContentLoaded", this._onPageReady.bind(this));
-}
+  // Initialization routines
+  WebApp._onInitWebWorker = function (emitter) {
+    Nuvola.WebApp._onInitWebWorker.call(this, emitter)
 
-// Page is ready for magic
-WebApp._onPageReady = function()
-{
+    var state = document.readyState
+    if (state === 'interactive' || state === 'complete') {
+      this._onPageReady()
+    } else {
+      document.addEventListener('DOMContentLoaded', this._onPageReady.bind(this))
+    }
+  }
+
+  // Page is ready for magic
+  WebApp._onPageReady = function () {
     // Connect handler for signal ActionActivated
-    Nuvola.actions.connect("ActionActivated", this);
+    Nuvola.actions.connect('ActionActivated', this)
 
     // Start update routine
-    this.update();
-}
+    this.update()
+  }
 
-// Extract data from the web page
-WebApp.update = function()
-{
-    var state = PlaybackState.UNKNOWN;
+  // Extract data from the web page
+  WebApp.update = function () {
+    var state = PlaybackState.UNKNOWN
     var track = {
-        title: null,
-        artist: null,
-        album: null,
-        artLocation: null
+      title: null,
+      artist: null,
+      album: null,
+      artLocation: null
     }
-    
-    var controls = document.getElementById("controls");
-    if (controls && controls.classList.contains('started'))
-    {
-        try
-        {
-            if (this.getButtonEnabled(1))
-                state = PlaybackState.PAUSED;
-            else if (this.getButtonEnabled(2))
-                state = PlaybackState.PLAYING;
-            
-            track.title = controls.querySelector(".song-info .title").title;
-            track.artist = controls.querySelector(".song-info .artist").title;
-            var albumArt = controls.querySelector(".albumart");
-            track.album = albumArt.title;
-            albumArt = albumArt.getAttribute("cover");
-            
-            // TODO: Album art - it's necessary to download raw bytes and send it to Nuvola.
-            // track.artLocation = albumArt ? this.getAbsoluteURL(albumArt) : null;
+
+    var controls = document.getElementById('controls')
+    if (controls && controls.classList.contains('started')) {
+      try {
+        if (this.getButtonEnabled(1)) {
+          state = PlaybackState.PAUSED
+        } else if (this.getButtonEnabled(2)) {
+          state = PlaybackState.PLAYING
         }
-        catch (e)
-        {
-            Nuvola.log("Parsing error: {1}", e);
-        }
+
+        track.title = controls.querySelector('.song-info .title').title
+        track.artist = controls.querySelector('.song-info .artist').title
+        var albumArt = controls.querySelector('.albumart')
+        track.album = albumArt.title
+        albumArt = albumArt.getAttribute('cover')
+
+        // TODO: Album art - it's necessary to download raw bytes and send it to Nuvola.
+        // track.artLocation = albumArt ? this.getAbsoluteURL(albumArt) : null;
+      } catch (e) {
+        Nuvola.log('Parsing error: {1}', e)
+      }
     }
-    
-    this.state = state;
-    player.setPlaybackState(state);
-    player.setTrack(track);
-    player.setCanGoPrev(state !== PlaybackState.UNKNOWN);
-    player.setCanGoNext(state !== PlaybackState.UNKNOWN);
-    player.setCanPlay(this.getButtonEnabled(1));
-    player.setCanPause(this.getButtonEnabled(2));
+
+    this.state = state
+    player.setPlaybackState(state)
+    player.setTrack(track)
+    player.setCanGoPrev(state !== PlaybackState.UNKNOWN)
+    player.setCanGoNext(state !== PlaybackState.UNKNOWN)
+    player.setCanPlay(this.getButtonEnabled(1))
+    player.setCanPause(this.getButtonEnabled(2))
 
     // Schedule the next update
-    setTimeout(this.update.bind(this), 500);
-}
+    setTimeout(this.update.bind(this), 500)
+  }
 
-WebApp.getAbsoluteURL = function(path)
-{
-    var port = location.port ? location.port : (location.protocol === "https:" ? "443" : "80");
-    return Nuvola.format("{1}//{2}:{3}{4}", location.protocol, location.hostname, port, path || "/");
-}
+  WebApp.getAbsoluteURL = function (path) {
+    var port = window.location.port ? window.location.port : (window.location.protocol === 'https:' ? '443' : '80')
+    return Nuvola.format('{1}//{2}:{3}{4}', window.location.protocol, window.location.hostname, port, path || '/')
+  }
 
-WebApp.getButton = function(index)
-{
-    var buttons = document.querySelectorAll("#controls.started #play-controls img");
-    return buttons.length ? buttons[index] : null;
-}
+  WebApp.getButton = function (index) {
+    var buttons = document.querySelectorAll('#controls.started #play-controls img')
+    return buttons.length ? buttons[index] : null
+  }
 
-WebApp.getButtonEnabled = function(index)
-{
-    var button = this.getButton(index);
-    return button ? !button.classList.contains('ng-hide') : false;
-}
+  WebApp.getButtonEnabled = function (index) {
+    var button = this.getButton(index)
+    return button ? !button.classList.contains('ng-hide') : false
+  }
 
 // Handler of playback actions
-WebApp._onActionActivated = function(emitter, name, param)
-{
-    switch (name)
-    {
-    case PlayerAction.TOGGLE_PLAY:
-    case PlayerAction.PLAY:
-    case PlayerAction.PAUSE:
-    case PlayerAction.STOP:
-        Nuvola.clickOnElement(this.getButton(1));
-        break;
-    case PlayerAction.PREV_SONG:
-        Nuvola.clickOnElement(this.getButton(0));
-        break;
-    case PlayerAction.NEXT_SONG:
-        Nuvola.clickOnElement(this.getButton(3));
-        break;
+  WebApp._onActionActivated = function (emitter, name, param) {
+    switch (name) {
+      case PlayerAction.TOGGLE_PLAY:
+      case PlayerAction.PLAY:
+      case PlayerAction.PAUSE:
+      case PlayerAction.STOP:
+        Nuvola.clickOnElement(this.getButton(1))
+        break
+      case PlayerAction.PREV_SONG:
+        Nuvola.clickOnElement(this.getButton(0))
+        break
+      case PlayerAction.NEXT_SONG:
+        Nuvola.clickOnElement(this.getButton(3))
+        break
     }
-}
+  }
 
-WebApp.start();
-
-})(this);  // function(Nuvola)
+  WebApp.start()
+})(this)  // function (Nuvola)
